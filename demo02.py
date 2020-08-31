e10fba1af897dff2fc0c52f5dc87534217303ecf
wuxumin <928390749@qq.com>
2020-08-31 22:31:46 +0800

    同上

--- demo02.py
+++ demo02.py
@@ -31,11 +31,6 @@ if a in "你好,111":
         print("33")
 """
 #循环
-a=1
-while a<10:
-    print("哈哈哈",a)
-    a=a+2
-
 """
 现在又10个学生的成绩，需录入系统中
 这个人分别是，张三，李四，王麻子，浪晋，刘云，嘻嘻，小梁，小小，二狗，小猪
@@ -66,3 +61,39 @@ for i  in a:
 b=list(range(0,100,3))  #自动生成一个数列 步进
 print(b)
 """
+#c=("嘿嘿","花花")
+# b=("aaa","bbb","cccc","mmmm")
+# a=(1,2,3,4,"哈哈","嘻嘻",True,None,b)
+#少写几个变量
+#每个变量都会占用内存，变量越多，占的越多
+#下标就是计算机自动编号，正向从0开始,反向从-1开始
+#print(a[4])
+#print(a[-7]) None表示-1 true表示1
+#print(a.index(4))
+#print(a.count(1))
+#二维元组，三维元组
+#切片，左闭右开
+# print(a[-2])
+#print(a[-1][4][0]) 
+# print(a[0:3])
+
+b=("aaa","bbb","cccc","mmmm")
+a=[1,2,3,4,"哈哈","嘻嘻",True,None,b]
+#print(a)
+
+# 元组和数组的操作方式一模一样
+# 区别是元组不可以修改，数组可以修改
+# name = input("请输入名字：")
+# a.append(name)
+# # print(a)
+# a.insert(3,name)
+# #print(a)
+# xx=a.pop(8)
+# print(xx)
+# c=["o","p","q"]
+# a.extend(c)
+# print(a)
+
+d=[1,3,4,5,2]
+d.sort(reverse=False)
+print(d)
